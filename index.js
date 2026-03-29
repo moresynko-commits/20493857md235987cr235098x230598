dedaeeb13565c0038daccc00c79e7acd84ffca6b
moresynko-commits <moresynko@gmail.com>
2026-03-29 13:41:48 -0500

    Update index.js

--- index.js
+++ index.js
@@ -118,5 +118,17 @@ async function sendVerifyReminder() {
     }
 }
 
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('MDCRX Services Discord bot is running!');
+});
+
+app.listen(port, () => {
+  console.log(`HTTP server listening on port ${port}`);
+});
+
 client.login(process.env.BOT_TOKEN);
 

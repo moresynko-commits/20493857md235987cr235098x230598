e7b403a042025f638f9aec6ae18d9760e2ccbc36
moresynko-commits <moresynko@gmail.com>
2026-03-29 21:22:32 -0500

    Create Log.js

---
+++ Log.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const logSchema = new mongoose.Schema({
+  type: String,
+  action: String,
+  userId: String,
+  playerData: mongoose.Schema.Types.Mixed,
+  timestamp: { type: Date, default: Date.now },
+  guildId: String
+});
+
+module.exports = mongoose.model('Log', logSchema);
